efdb72f72f8e9d2b1645264986a54d6119375257
Chaitya Mehta <PCQ39@SERVER7.COM>
2025-11-13 11:01:21 +0530

    remove-comment

--- server.js
+++ server.js
@@ -42,34 +42,6 @@ app.use(/.*/, (req, res) => {
   });
 });
 app.use(errorHandler);
-// app.use((err, req, res) => {
-//   console.error(err.stack);
-//   if (err.name === 'ValidationError') {
-//     const messages = Object.values(err.errors).map((val) => val.message);
-//     return res.status(400).json({
-//       success: false,
-//       message: 'Validation Error',
-//       errors: messages
-//     });
-//   }
-//   if (err.code === 11000) {
-//     return res.status(400).json({
-//       success: false,
-//       message: 'Duplicate field value entered'
-//     });
-//   }
-//   if (err.name === 'CastError') {
-//     return res.status(400).json({
-//       success: false,
-//       message: 'Resource not found'
-//     });
-//   }
-//   res.status(err.statusCode || 500).json({
-//     success: false,
-//     message: err.message || 'Server Error',
-//     error: process.env.NODE_ENV === 'production' ? {} : err.stack
-//   });
-// });
 
 const PORT = process.env.PORT || 5000;
 
